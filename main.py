ec576ddfd26cce04211d8ee3ffe7504708c59172
Anh Do <anhdotuan.mail@gmail.com>
2021-08-10 22:35:04 -0400

    Minor edit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Anh Do (Student ID #001511153)
-import readPackage
-from readPackage import get_package_hash_table
+from readData import get_package_hash_table
 
 print('WGUPS Routing Program')
 package = int(input('Status for package: '))
