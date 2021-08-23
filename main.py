47e525574b8f1ac0a04926a464b6657ca0fd39eb
Anh Do <anhdotuan.mail@gmail.com>
2021-08-22 23:45:05 -0400

    Minor guiding comments

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 # Anh Do (Student ID #001511153)
-from readData import get_package_hash_table
+import readData
+import optimize
+
+'''
+    INTERFACE REQUIREMENTS:
+    1. Status/Info of any package at any time
+    2. Total mileage traveled by all trucks
+    3. Status of all packages at a time between:
+        a. 08:35 am  - 09:25 am
+        b. 09:35 am  - 10:25 am
+        c. 12:03 pm  - 01:12 pm
+'''
 
 print('WGUPS Routing Program')
-package = int(input('Status for package: '))
+# package = int(input('Status for package: '))
 
-packageHashTable = get_package_hash_table()
-print(packageHashTable.get(package))
+# run optimize module
+
+# provide interface with options
+
+
+
 
 
